f8bae0152bec27ed082fe1a0e0374b7dbcb24c20
kiliman <kiliman@gmail.com>
2024-04-28 08:26:38 -0400

    🐛 Cannot find build/server/remix.js
    Fix #1

--- src/express.ts
+++ src/express.ts
@@ -63,7 +63,7 @@ export function createExpressApp({
       ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         // eslint-disable-next-line import/no-unresolved -- this expected until you build the app
-        await import('../build/server/remix.js')
+        await import(process.cwd() + '/build/server/remix.js')
       : await importDevBuild()) as unknown as ServerBuild
 
     return createRequestHandler({ build, mode, getLoadContext })(req, res, next)
